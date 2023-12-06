6c76ca01bd3bf3833fa93ef7a4420f9b352e036d
lowzyyy <peratheking@aol.com>
2023-12-06 11:10:22 +0100

    Don't respond to old messages when bot was offline

--- src/app.js
+++ src/app.js
@@ -27,6 +27,8 @@ const bot = new Telegraf(process.env.TOKEN);
 
 // short price | price of tokens
 bot.command("p", async (ctx) => {
+  // if message is older than 10 min, dont respond
+  if (new Date().getTime() - ctx.message.date > 600_000) return;
   let [tokenOption, ...rest] = ctx.message.text.split(" ").slice(1);
   const tokenSet = new Set(tokenList.map((t) => t.symbol));
   let data;
